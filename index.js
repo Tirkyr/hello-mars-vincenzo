b29a79d39a580ddffccd40f8e0479a28b1b532eb
Vincenzo <vincenzo.delluniversit@virgilio.it>
2023-02-23 19:08:22 +0100

    beta version with languages

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = function helloMars() {
-  console.log("Hello Mars!");
+module.exports = function helloMars(lang) {
+  if (lang == "es") {
+    console.log("¡Hola Marte!");
+  } else {
+    console.log("Hello Mars!");
+  }
 };
